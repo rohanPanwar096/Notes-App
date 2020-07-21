552300cf216ec135ee1a186714a19b7a91158da4
Rohan Panwar <rohan.panwar@neurontechlabs.com>
2020-07-21 22:29:55 +0530

    Final Commit

--- notes.js
+++ notes.js
@@ -1,93 +1,84 @@
-const fs = require('fs')
-const chalk = require('chalk')
-
+const fs = require("fs");
+const chalk = require("chalk");
 
 const addNote = (title, body) => {
-    const notes = loadNote()
-    const duplicateNote = notes.find(note => note.title === title)
-    
-    if(!duplicateNote){
+  const notes = loadNote();
+  const duplicateNote = notes.find((note) => note.title === title);
 
-      notes.push({
-        title: title,
-        body: body  
-      })
-      
-      saveNotes(notes)
-      console.log('New note added')
-    }else{
-       console.log('Note title already exists')
-    }
-}
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body,
+    });
 
+    saveNotes(notes);
+    console.log("New note added");
+  } else {
+    console.log("Note title already exists");
+  }
+};
 
-
-const saveNotes = notes => {
-    const dataJson = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJson)
-}
+const saveNotes = (notes) => {
+  const dataJson = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJson);
+};
 
 const loadNote = () => {
-    try{
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJson = dataBuffer.toString()
-        return JSON.parse(dataJson) //Array of objects 
-    }catch(e){
-        return []
-    }
-}
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson); //Array of objects
+  } catch (e) {
+    return [];
+  }
+};
 
 const listNotes = () => {
-   const notes = loadNote()
+  const notes = loadNote();
 
-   notes.forEach(e => {
-      console.log(`${e.title}`)
-   })
-   
-//    const notesToShow = notes.filter(n => {
-//        return n.title === title
-//    })
-}
+  notes.forEach((e) => {
+    console.log(`${e.title}`);
+  });
 
+  //    const notesToShow = notes.filter(n => {
+  //        return n.title === title
+  //    })
+};
 
-const readNote = title => {
-    const note = loadNote()
-    
-    const notes = note.find(e => e.title === title)
+const readNote = (title) => {
+  const note = loadNote();
 
-    if(notes){
-        console.log(chalk.inverse(notes.title), notes.body)
-    }else{
-        console.log(chalk.red.inverse("No Note Found"))
-    }
-}
+  const notes = note.find((e) => e.title === title);
 
-const removeNote = title => {
-    //  const remBuffer = fs.readFileSync('notes.json')
-    //  const remJson = remBuffer.toString()
-    //  const rem = JSON.parse(remJson)
-    //  rem.title = ""
-    const rem = loadNote()
-    
-    const notesToKeep = rem.filter(note => {
-       return note.title !== title
-    })
-    
-    if(rem.length > notesToKeep.length){
-        console.log(chalk.greenBright('Note is removed')) 
-        saveNotes(notesToKeep)
-    }else{
-        console.log(chalk.redBright('No Note Found'))
-    }
-   
-}
+  if (notes) {
+    console.log(chalk.green(notes.title), notes.body);
+  } else {
+    console.log(chalk.red.inverse("No Note Found"));
+  }
+};
 
+const removeNote = (title) => {
+  //  const remBuffer = fs.readFileSync('notes.json')
+  //  const remJson = remBuffer.toString()
+  //  const rem = JSON.parse(remJson)
+  //  rem.title = ""
+  const rem = loadNote();
+
+  const notesToKeep = rem.filter((note) => {
+    return note.title !== title;
+  });
+
+  if (rem.length > notesToKeep.length) {
+    console.log(chalk.greenBright("Note is removed"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.redBright("No Note Found"));
+  }
+};
 
 module.exports = {
-    getNotes: getNotes,
-    addNote: addNote,
-    removeNote: removeNote,
-    listNotes: listNotes,
-    readNote: readNote
-}
-
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
+};
