5feaeed308fb65478c8a4c420a79ba2fbdb5fb0b
Rohan Panwar <rohan.panwar@neurontechlabs.com>
2020-07-21 22:55:34 +0530

    Modify App file

--- app.js
+++ app.js
@@ -1,85 +1,64 @@
-const notes = require('./notes.js')
-const yargs = require('yargs')
+const notes = require("./notes.js");
+const yargs = require("yargs");
 
 //create an add command
 
 yargs.command({
-    command: 'add',
-    builder: {
-        title: {
-           describe: 'Note title',
-           demandOption: true,
-           type: 'string' 
-        },
-        body: {
-            describe: 'Add a new Note',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "add",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
     },
-    handler: argv => {
-        notes.addNote(argv.title, argv.body)
-    }
-})
+    body: {
+      describe: "Add a new Note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    notes.addNote(argv.title, argv.body);
+  },
+});
 
 //create remove command
 
 yargs.command({
-    command: 'remove',
-    builder: {
-        title: {
-           describe: 'Remove a note',
-           demandOption: true,
-           type: 'string'
-        }
+  command: "remove",
+  builder: {
+    title: {
+      describe: "Remove a note",
+      demandOption: true,
+      type: "string",
     },
-    handler: argv => {
-        notes.removeNote(argv.title)
-    }
-})
+  },
+  handler: (argv) => {
+    notes.removeNote(argv.title);
+  },
+});
 
 //create read command
- yargs.command({
-    command: 'read',
-    builder: {
-        title: {
-            describe: 'Read a note',
-            demandOption: true,
-            type: 'string'
-        }
+yargs.command({
+  command: "read",
+  builder: {
+    title: {
+      describe: "Read a note",
+      demandOption: true,
+      type: "string",
     },
-    handler: argv => {
-        notes.readNote(argv.title)
-    }
-})
+  },
+  handler: (argv) => {
+    notes.readNote(argv.title);
+  },
+});
 
- yargs.command({
-    command: 'List',
-    describe: 'Listing Notes',
-    handler: () => { 
-        notes.listNotes()
-    }
-})
+yargs.command({
+  command: "list",
+  describe: "Listing Notes",
+  handler: () => {
+    notes.listNotes();
+  },
+});
 
- yargs.parse()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+yargs.parse();
